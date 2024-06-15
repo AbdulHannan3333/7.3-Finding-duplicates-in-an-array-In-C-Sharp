272f35405e868a7ec4d43a4d20739735fe662f2d
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:34:57 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3_Finding_duplicates_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1,2,3,5,8,1,1,3,8,10,6,2};
+            Console.WriteLine("Duplicate Numbers");
+            for (int i = 0; i < number.Length; i++) 
+            {
+                for (int j = i + 1; j < number.Length; j++) 
+                {
+                    if (number[i] == number[j]) 
+                    {
+                        Console.WriteLine($"{number[i]}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
